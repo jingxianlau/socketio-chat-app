dbc7c4cd24185bae873f86c3cf340196a61491e7
jingxianlau <jingxian.lau16@gmail.com>
2023-05-07 20:49:03 +0800

    cors

--- server/dist/server.js
+++ server/dist/server.js
@@ -5,8 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    origin: 'http://localhost:3000'
+}));
 app.get('/', (req, res) => {
     res.send('API is Running');
 });
